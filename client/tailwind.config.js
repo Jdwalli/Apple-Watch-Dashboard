a63f68089eac45ee0993ac60efc9ee2e903782cb
Joshua <Jdwalli2003@yahoo.com>
2022-07-13 20:35:16 -0400

    Changed tailwind and ts configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14141A",
+        secondary: "#292932",
+        red: { DEFAULT : "#EF4444" },
+        green: { DEFAULT : "#2BFEBE" },
+        white: { DEFAULT : "#F2F0F0" }
+      }
+    },
   },
   plugins: [],
 }

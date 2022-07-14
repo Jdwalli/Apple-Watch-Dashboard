2b06655039c3da54a6d45d0f00b5a8222beb1cec
Joshua <Jdwalli2003@yahoo.com>
2022-07-13 20:46:19 -0400

    Added navigation components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { VerticalNav } from './components/navigation/VerticalNav';
+import { Header } from './components/common/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div> </div>
+    <Router>
+      <div className='h-screen w-screen bg-yellow-300'>
+      <VerticalNav /> 
+      <div className="h-screen bg-primary ml-24">
+        <Header />
+        <Routes>
+            <Route />
+          </Routes>
+      </div>
+
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
